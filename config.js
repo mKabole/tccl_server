c32ae292a7fb6e50da03241c79be89366d675546
mKabole <makungukabole34@gmail.com>
2023-12-21 19:43:43 +0200

    changed db config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     db: {
       /* don't expose password or any sensitive info, done only for demo */
-      host: "http://196.46.192.209",
-      user: "phpmyadmin",
-      password: "209@Tccl2023",
+      host: "http://196.46.192.214",
+      user: "root",
+      password: "214@Tccl2023",
       database: "tccl_db",
       connectTimeout: 60000
     },
